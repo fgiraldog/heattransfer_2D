9aa0ce50209c040a603e2dedcf9fcf72a3e08883
fgiraldog <f.giraldo@uniandes.edu.co>
2020-04-23 18:36:48 -0500

    Ecuaciones corregidas

--- ht_2d.py
+++ ht_2d.py
@@ -1,17 +1,21 @@
 from sympy import linear_eq_to_matrix, MatrixSymbol, Eq, solve_linear_system
 from sympy import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 q = 1.
 h = 1.
-t_f = 30.
+t_f = 5.
+h_f = 1.
 delta = 0.05
 h = 1.*delta
 n = int(1/delta) + 1 # numero de nodos
+
 t = symarray('t',(n,n)) # nodos
 
 equations = []
 
+counter = 0
 for i in range(0,n):
 	for j in range(0,n):
 
@@ -19,55 +23,99 @@ for i in range(0,n):
 		if j == 0: # frontera vertical izq
 			if i == 0: # punto superior
 				equations.append(Eq(t[i+1,j] + t[i,j+1] - (2*t[i,j]*(1+h)),0))
+				counter+=1
 
-			if i*delta > 0.75: # punto inferior
+			elif i*delta > 0.75: # punto inferior
 				equations.append(Eq(t[i,j],t_f))
+				counter+=1
+
+			elif i*delta == 0.75:
+				equations.append(Eq(t[i-1,j] + t[i,j+1] - (t[i,j]*(2+h+h_f)),0))
+				counter+=1
 
 			else: # demas
 				equations.append(Eq(2*t[i,j+1] + t[i+1,j] + t[i-1,j] - 2*t[i,j]*(2+h),0))
+				counter+=1
 
 		if j == n-1: # frontera vertical der
 			if i == 0: # punto superior
 				equations.append(Eq(t[i+1,j] + t[i,j-1] - (2*t[i,j]*(1+h)),0))
+				counter+=1
 
-			if i == n-1: # punto inferior
+			elif i == n-1: # punto inferior
 				equations.append(Eq(t[i-1,j] + t[i,j-1] - (2*t[i,j]*(1+h)),0))
+				counter+=1
 
 			else: # demas
 				equations.append(Eq(2*t[i,j-1] + t[i+1,j] + t[i-1,j] - 2*t[i,j]*(2+h),0))
+				counter+=1
 
 		if i == 0: # frontera horizontal sup
 			if j > 0 and j < n-1: # demas
 				equations.append(Eq(2*t[i+1,j] + t[i,j-1] + t[i,j+1] - 2*t[i,j]*(2+h),0))
+				counter+=1
 
-		if i == n-1:
-			if j*delta < 0.25:
+		if i == n-1: # frontera horizontal inf
+			if j*delta < 0.25 and j*delta > 0:
 				equations.append(Eq(t[i,j],t_f))
+				counter+=1
 
-			if j*delta >= 0.25 and j < n-1: # demas
+			elif j*delta > 0.25 and j < n-1: # demas
 				equations.append(Eq(2*t[i-1,j] + t[i,j-1] + t[i,j+1] - 2*t[i,j]*(2+h),0))
+				counter+=1
+
+			elif j*delta == 0.25:
+				equations.append(Eq(t[i-1,j] + t[i,j+1] - (t[i,j]*(2+h+h_f)),0))
+				counter+=1
+
+
+		#Frontera rara
+		if i*delta == 0.75 and j > 0 and j*delta < 0.25:
+			equations.append(Eq(2*t[i-1,j] + t[i,j-1] + t[i,j+1] - 2*t[i,j]*(2+h_f),0))
+			counter+=1
+
+		if j*delta == 0.25 and i < n-1 and i*delta > 0.75:
+			equations.append(Eq(t[i+1,j] + t[i,j+1] - (2*t[i,j]*(1+h_f)),0))
+			counter+=1
+
+		if i*delta == 0.75 and j*delta == 0.25:
+			equations.append(Eq(t[i-1,j] + t[i,j+1] - (2*t[i,j]*(1+h)),0))
+			counter+=1
+
 
 		# Nodos internos
-		if i < 0 and i*delta < 0.75 and j < 0 and j*delta <= 0.25: 
+		if i > 0 and i*delta < 0.75 and j > 0 and j*delta <= 0.25: 
 			if i*delta < 0.25 or i*delta > 0.5:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],0))
+				counter+=1
 
 			else:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],-q))
+				counter+=1
 
-		if i > n-1 and i*delta >= 0.75 and j > n-1 and j*delta > 0.25:
+		if i < n-1 and i*delta >= 0.75 and j < n-1 and j*delta > 0.25:
 			if j*delta < 0.5 or j*delta > 0.75:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],0))
+				counter+=1
 
 			else:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],-q))
+				counter+=1
 
-		if i < 0 and i*delta < 0.75 and j*delta > 0.25 and j < n-1:
+		if i > 0 and i*delta < 0.75 and j*delta > 0.25 and j < n-1:
 			if i*delta < 0.25 or i*delta > 0.5 or j*delta < 0.5 or j*delta > 0.75:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],0))
+				counter+=1
 			else:
 				equations.append(Eq(t[i+1,j] + t[i-1,j] + t[i,j+1] + t[i,j-1] - 4*t[i,j],-q))
+				counter+=1
 
+		if i*delta > 0.75 and i < n-1 and j*delta < 0.25 and j > 0:
+			equations.append(Eq(t[i,j],t_f))
+			counter+=1
+
+
+print(counter)
 variables = []
 for i in range(0,n):
 	for j in range(0,n):
@@ -83,4 +131,5 @@ t = np.around(np.linalg.solve(a,b), decimals = 3)
 
 plt.figure()
 plt.imshow(t.reshape(n,n))
+plt.colorbar()
 plt.show()
